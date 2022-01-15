9ceaa57cb3e084c7f0a077edf448404780b5fc91
Lillian Raphael <42048931+cybersnail-ui@users.noreply.github.com>
2022-01-15 16:48:17 -0500

    fix scene 1
    fix scene 1

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,5 @@
 const controller = new ScrollMagic.Controller();
 
-
 const scene1 = new TimelineLite();
 
 scene1.add(
@@ -11,11 +10,11 @@ scene1.add(
 );
 
 const scene2 = new ScrollMagic.Scene({
-    triggerElement: '.three',
+    triggerElement: '.one',
     duration: 1000,
     triggerHook: 0,
 })
 .setTween(scene1)
 .addIndicators()
-.setPin('.three')
+.setPin('.one')
 .addTo(controller)
