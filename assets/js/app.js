e141f52dd0ac8abc06bdda0a4586d230cafe60b4
Lillian Raphael <42048931+cybersnail-ui@users.noreply.github.com>
2022-04-25 15:47:39 -0400

    update scroll
    update scroll

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,7 +5,7 @@ const scene1 = new TimelineLite();
 scene1.add(
     TweenLite.to(".scroll-down", 1,  {
         css:{ opacity: 0 },
-    }),
+    })
 )
 .add(
     TweenLite.to(".alive", 1,  {
@@ -20,7 +20,7 @@ scene1.add(
 
 const scene2 = new ScrollMagic.Scene({
     triggerElement: '.scene-one',
-    duration: 750,
+    duration: 500,
     triggerHook: 0,
 })
 .setTween(scene1)
@@ -28,3 +28,115 @@ const scene2 = new ScrollMagic.Scene({
 .addTo(controller)
 
 //.addIndicators()
+
+new ScrollMagic.Scene({
+    triggerElement: "#about-sec",
+    triggerHook: 0.98,
+})
+.setClassToggle("#about-sec", "show") // add class to reveal
+.addTo(controller);
+
+new ScrollMagic.Scene({
+    triggerElement: "#about-1",
+    triggerHook: 0.98,
+})
+.setClassToggle("#about-1", "show") // add class to reveal
+.addTo(controller);
+
+new ScrollMagic.Scene({
+    triggerElement: "#about-2",
+    triggerHook: 0.98,
+})
+.setClassToggle("#about-2", "show") // add class to reveal
+.addTo(controller);
+
+new ScrollMagic.Scene({
+    triggerElement: "#about-3",
+    triggerHook: 0.98,
+})
+.setClassToggle("#about-3", "show") // add class to reveal
+.addTo(controller);
+
+new ScrollMagic.Scene({
+    triggerElement: "#about-4",
+    triggerHook: 0.98,
+})
+.setClassToggle("#about-4", "show") // add class to reveal
+.addTo(controller);
+
+new ScrollMagic.Scene({
+    triggerElement: "#video-sec",
+    triggerHook: 0.98,
+})
+.setClassToggle("#video-sec", "show") // add class to reveal
+.addTo(controller);
+
+new ScrollMagic.Scene({
+    triggerElement: "#gallery-sec",
+    triggerHook: 0.98,
+})
+.setClassToggle("#gallery-sec", "show") // add class to reveal
+.addTo(controller);
+
+new ScrollMagic.Scene({
+    triggerElement: "#character-sec",
+    triggerHook: 0.98,
+})
+.setClassToggle("#character-sec", "show") // add class to reveal
+.addTo(controller);
+
+new ScrollMagic.Scene({
+    triggerElement: "#concept-sec",
+    triggerHook: 0.98,
+})
+.setClassToggle("#concept-sec", "show") // add class to reveal
+.addTo(controller);
+
+
+
+
+/* const downloadScene = new TimelineLite();
+
+downloadScene.add(
+    TweenLite.to("#d-1", .15,  {
+        css:{ opacity: 1 },
+        ease: Power2.easeIn,
+    })
+)
+.add(
+    TweenLite.to("#d-2", .15,  {
+        css:{ opacity: 1 },
+        ease: Power2.easeIn,
+    })
+)
+.add(
+    TweenLite.to("#d-3", .15,  {
+        css:{ opacity: 1 },
+        ease: Power2.easeIn,
+    })
+)
+.add(
+    TweenLite.to("#d-4", .15,  {
+        css:{ opacity: 1 },
+        ease: Power2.easeIn,
+    })
+)
+.add(
+    TweenLite.to("#d-5", .15,  {
+        css:{ opacity: 1 },
+        ease: Power2.easeIn,
+    })
+)
+.add(
+    TweenLite.to("#d-6", .15,  {
+        css:{ opacity: 1 },
+        ease: Power2.easeIn,
+    })
+) */
+
+new ScrollMagic.Scene({
+    triggerElement: "#download-sec",
+    triggerHook: 0.98,
+})
+.setClassToggle("#download-sec", "show") // add class to reveal
+.addTo(controller);
